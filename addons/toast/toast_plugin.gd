fa70f2afb856cbc1514df4053ecfe7c894b4bbd7
Rares Vanca <raresvanca@gmail.com>
2020-12-30 13:32:32 +0200

    New Icons

--- addons/toast/toast_plugin.gd
+++ addons/toast/toast_plugin.gd
@@ -3,8 +3,8 @@ extends EditorPlugin
 
 
 func _enter_tree():
-	add_custom_type("Toast", "Control", preload("toast_script.gd"), preload("icon.png"))
-	add_custom_type("Toast Style", "Resource", preload("style_resource/toast_style.gd"), preload("style_resource/icon.svg"))
+	add_custom_type("Toast", "Control", preload("toast_script.gd"), preload("icons/Toast.svg"))
+	add_custom_type("Toast Style", "Resource", preload("style_resource/toast_style.gd"), preload("icons/ToastStyle.svg"))
 
 
 func _exit_tree():
